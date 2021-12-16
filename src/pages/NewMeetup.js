1c8f73099020e3d2f6de3b67091555419c854f42
HJ <de0978@gmail.com>
2021-12-16 23:14:07 +0900

    add 사용자 입력 받기 및 양식 제출 처리

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetupPage = () => {
     return(
         <section>
             <h1>Add New Meetup</h1>
+            <NewMeetupForm/>
         </section>
     )
 }
